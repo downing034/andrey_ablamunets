d499857420946227ce18511c485a6f1875dc3793
Paul Downing <paul.downing@govdelivery.com>
2017-03-28 21:51:54 -0500

    added internal admin status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226195813) do
+ActiveRecord::Schema.define(version: 20170329023327) do
+
+  create_table "blog_posts", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "date"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170226195813) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "internal"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
