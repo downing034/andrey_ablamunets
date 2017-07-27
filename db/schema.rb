e90c226b677ca1acfe7c171f5e49fac8705b3a16
Paul Downing <downing034@gmail.com>
2017-07-26 22:38:28 -0500

    added lots of extra options to the editor, still need to render it correctly, and add image/video hosting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529203901) do
+ActiveRecord::Schema.define(version: 20170529202720) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
